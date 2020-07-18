2330bc323c16b3fd48b2e454f7004e7a23a35a73
Carlos Fuentes <fuentesc91@gmail.com>
2020-07-18 14:40:48 -0500

    Fix issues with navbar and position of elements

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import MainSlider from './components/MainSlider';
 
+// Test
+import SectionTitle from './components/section-title/SectionTitle';
+
 import './app.scss';
 
 const App = () => {
@@ -10,6 +13,7 @@ const App = () => {
     <div className="site-container container-fluid">
       <Navbar />
       <MainSlider />
+      <SectionTitle />
     </div>
   );
 };
---
+++ src/components/section-title/SectionTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionTitle = () => {
+  return (
+    <div>
+      <p>Section Title</p>
+    </div>
+  );
+};
+
+export default SectionTitle;
