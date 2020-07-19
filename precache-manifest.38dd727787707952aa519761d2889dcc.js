8c4f114abacfca43e846b1747b4b07fa89d23952
Carlos Fuentes <fuentesc91@gmail.com>
2020-07-19 11:05:18 -0500

    Updates

---
+++ precache-manifest.38dd727787707952aa519761d2889dcc.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ace09e796c2973cbf93f81a9d6fbf5",
+    "url": "/baby-photo-me/index.html"
+  },
+  {
+    "revision": "86a7bb9cc4932b4726a5",
+    "url": "/baby-photo-me/static/css/main.0bb5e0bb.chunk.css"
+  },
+  {
+    "revision": "ba0d5d446e61698d2f08",
+    "url": "/baby-photo-me/static/js/2.9a844520.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/baby-photo-me/static/js/2.9a844520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a7bb9cc4932b4726a5",
+    "url": "/baby-photo-me/static/js/main.ed4376dd.chunk.js"
+  },
+  {
+    "revision": "64375af4a32c9e7d8df7",
+    "url": "/baby-photo-me/static/js/runtime-main.7369ebce.js"
+  },
+  {
+    "revision": "8dddfc4c12cf25d9913689b1969523d7",
+    "url": "/baby-photo-me/static/media/AlteHaasGroteskBold.8dddfc4c.ttf"
+  },
+  {
+    "revision": "38025932dce48e268882d955f7a66826",
+    "url": "/baby-photo-me/static/media/AlteHaasGroteskRegular.38025932.ttf"
+  },
+  {
+    "revision": "134ec6ccbafe16d6a1e3cb055d911129",
+    "url": "/baby-photo-me/static/media/Charline font.134ec6cc.otf"
+  },
+  {
+    "revision": "5a8d221a73845d2e88c154459c3af2d1",
+    "url": "/baby-photo-me/static/media/baby01.5a8d221a.jpg"
+  },
+  {
+    "revision": "6dd64dc8958dc52502983a67bb7e73c5",
+    "url": "/baby-photo-me/static/media/baby02.6dd64dc8.jpg"
+  },
+  {
+    "revision": "98569ecb171ed40e68288c1c1481fb8b",
+    "url": "/baby-photo-me/static/media/baby03.98569ecb.jpg"
+  },
+  {
+    "revision": "870d7c686a71b5f04cce3c18384cdb91",
+    "url": "/baby-photo-me/static/media/baby04.870d7c68.jpg"
+  },
+  {
+    "revision": "57a1cfc83adb4e1e89ce436d1501ac6f",
+    "url": "/baby-photo-me/static/media/baby05.57a1cfc8.jpg"
+  },
+  {
+    "revision": "da6a00bfc988fb30a0c73b8417f619dc",
+    "url": "/baby-photo-me/static/media/baby06.da6a00bf.jpg"
+  },
+  {
+    "revision": "38b47b778672ee9ecd982db0247888f2",
+    "url": "/baby-photo-me/static/media/bautizo01.38b47b77.jpg"
+  },
+  {
+    "revision": "83a1f95ed1c8ad54c8d654d078ee29c5",
+    "url": "/baby-photo-me/static/media/bautizo02.83a1f95e.jpg"
+  },
+  {
+    "revision": "c36e7d17f396de6e7da667e997c5c68c",
+    "url": "/baby-photo-me/static/media/bautizo03.c36e7d17.jpg"
+  },
+  {
+    "revision": "b2127d2ba2956ff75106c9ec4ba10b19",
+    "url": "/baby-photo-me/static/media/bautizo04.b2127d2b.jpg"
+  },
+  {
+    "revision": "98b0a95431abfc012a65940356768124",
+    "url": "/baby-photo-me/static/media/embarazo01.98b0a954.jpg"
+  },
+  {
+    "revision": "80241854580e780865657a79a1e61601",
+    "url": "/baby-photo-me/static/media/embarazo02.80241854.jpg"
+  },
+  {
+    "revision": "c011ce5e996046d8bea8cc8bfc9c5e15",
+    "url": "/baby-photo-me/static/media/embarazo03.c011ce5e.jpg"
+  },
+  {
+    "revision": "a5c1a18f09876a4e68d7096fe01fb65b",
+    "url": "/baby-photo-me/static/media/embarazo04.a5c1a18f.jpg"
+  },
+  {
+    "revision": "60926c1eb229b1946344aa10aef72fb7",
+    "url": "/baby-photo-me/static/media/embarazo05.60926c1e.jpg"
+  },
+  {
+    "revision": "c5a44558a1df67dbd6415b970985c256",
+    "url": "/baby-photo-me/static/media/embarazo06.c5a44558.jpg"
+  },
+  {
+    "revision": "6cfe88432515f9904cc29ffc180e3e6f",
+    "url": "/baby-photo-me/static/media/newborn01.6cfe8843.jpg"
+  },
+  {
+    "revision": "136943358ea9131dd5f9ae35342c4144",
+    "url": "/baby-photo-me/static/media/newborn02.13694335.jpg"
+  },
+  {
+    "revision": "9910f7d62f6dc2abc590fc5078dd27a5",
+    "url": "/baby-photo-me/static/media/newborn03.9910f7d6.jpg"
+  },
+  {
+    "revision": "bf0b7bdf6677aa14c252dafe84fb1a89",
+    "url": "/baby-photo-me/static/media/newborn04.bf0b7bdf.jpg"
+  },
+  {
+    "revision": "fae15b9b33c376a3170f4888f99f7e09",
+    "url": "/baby-photo-me/static/media/slider01.fae15b9b.jpg"
+  },
+  {
+    "revision": "c9287e450464bf577e4261dc1af3a1a8",
+    "url": "/baby-photo-me/static/media/slider02.c9287e45.jpg"
+  },
+  {
+    "revision": "a5c1a18f09876a4e68d7096fe01fb65b",
+    "url": "/baby-photo-me/static/media/slider03.a5c1a18f.jpg"
+  },
+  {
+    "revision": "0a1f3dd67940b86861ea80ad715a377a",
+    "url": "/baby-photo-me/static/media/slider04.0a1f3dd6.jpg"
+  },
+  {
+    "revision": "8b09ebe3eb4d6a1bd6875de11f41b61c",
+    "url": "/baby-photo-me/static/media/slider05.8b09ebe3.jpg"
+  },
+  {
+    "revision": "ae0e94de001da20f5d88b49c563995a4",
+    "url": "/baby-photo-me/static/media/smash01.ae0e94de.jpg"
+  },
+  {
+    "revision": "6de45e3e688ba438993524afb4315112",
+    "url": "/baby-photo-me/static/media/smash02.6de45e3e.jpg"
+  },
+  {
+    "revision": "444d40a3716629b5a88f2af8e8af02fb",
+    "url": "/baby-photo-me/static/media/smash03.444d40a3.jpg"
+  },
+  {
+    "revision": "0a1f3dd67940b86861ea80ad715a377a",
+    "url": "/baby-photo-me/static/media/smash04.0a1f3dd6.jpg"
+  },
+  {
+    "revision": "84e3933f4ae7aed731762ff1a3449fe1",
+    "url": "/baby-photo-me/static/media/smash05.84e3933f.jpg"
+  },
+  {
+    "revision": "a5373e80fecc8935f7cfa1fbf3ebc9b7",
+    "url": "/baby-photo-me/static/media/smash06.a5373e80.jpg"
+  }
+]);
